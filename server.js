c5733ae1fbfd9cb62eb6d863a73b3720239a7e73
Khalid <khalid@local.lan>
2024-01-14 18:26:55 -0500

    refactor: return only needed props

--- server.js
+++ server.js
@@ -10,7 +10,42 @@ app.use(cors());
 app.get('/api/starships', async (req, res) => {
   try {
     const response = await axios.get('https://swapi.dev/api/starships/');
-    const starships = response.data.results;
+    const starships = response.data.results.map((starship) => {
+        const {
+          name,
+          model,
+          manufacturer,
+          cost_in_credits,
+          length,
+          max_atmosphering_speed,
+          crew,
+          passengers,
+          cargo_capacity,
+          consumables,
+          hyperdrive_rating,
+          MGLT,
+          films,
+          pilots,
+        } = starship;
+  
+        return {
+          name,
+          model,
+          manufacturer,
+          cost_in_credits,
+          length,
+          max_atmosphering_speed,
+          crew,
+          passengers,
+          cargo_capacity,
+          consumables,
+          hyperdrive_rating,
+          MGLT,
+          films,
+          pilots,
+        };
+      });
+      
     res.json(starships);
   } catch (error) {
     console.error('Error fetching starships from SWAPI:', error.message);
